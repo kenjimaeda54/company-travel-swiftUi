5d8b438660d81f6882217bfbb747d76ccdaa38cf
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2023-09-10 15:09:09 -0300

    Initial Commit

---
+++ Company Travel/Company_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Company_TravelApp.swift
+//  Company Travel
+//
+//  Created by kenjimaeda on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Company_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
